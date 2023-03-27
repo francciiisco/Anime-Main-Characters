a8496afe2855622fa91b3b45b8b11a2d2a2efaec
momot <motraore611@yahoo.com>
2023-03-27 15:41:24 -0400

    components created

--- back/server.js
+++ back/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Anime = require('./models')
+const Anime = require('./models/anime.js')
 
 app.use(express.json());
 app.use(cors());
